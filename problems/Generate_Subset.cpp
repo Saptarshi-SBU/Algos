97ac221d95a1ebf6e54c1acd79e29b3f573251c1
Saptarshi-SBU <saptarshi.mrg@gmail.com>
2017-04-02 21:57:13 -0700

    Generate Subsets for a Set with Duplicate elements
    Signed-off-by: Saptarshi-SBU <saptarshi.mrg@gmail.com>

--- problems/Generate_Subset.cpp
+++ problems/Generate_Subset.cpp
@@ -2,13 +2,20 @@
  *
  * Copyright(C) : 2017, Saptarshi Sen
  * 
- * Generate Possible Subsets : Power Set (2^n)
+ * Generate Possible Subsets : Power Set 
  *
- * https://leetcode.com/submissions/detail/98891272/
+ * Note : The subsets are distinct
+ *
+ * Noduplictaes : https://leetcode.com/submissions/detail/98891272/ (2^N)
+ *
+ * Duplicates : https://leetcode.com/submissions/detail/98921691/
  *
  * ----------------------------------------------------------------------*/
 #include <vector>
+#include <unordered_map>
 #include <iostream>
+
+#define NODUPLICATES 0
 
 using namespace std;
 
@@ -54,6 +61,52 @@ class Solution {
             GenerateSubset(nums, result);
         }
 
+        // Treat Multiple Occurences as ONE and then add them to existing 
+        // subset entries.(and then expand)
+        vector<vector<int>> subsetsForDuplicates(vector<int>& nums) {
+
+            // Result stored
+            vector<vector<int>> result;
+
+            // map for duplicates
+            unordered_map<int, int> freq;
+
+            // for duplicates
+            for (auto& i : nums) {
+               freq[i] = (freq.find(i) == freq.end()) ? 1 : freq[i] + 1;
+            }   
+
+            // for all unique elements
+            for (auto& i : freq) {
+               // possible subsets from current element
+               vector<vector<int>> subset;
+           
+               // vector with occurences of current element 
+               vector<int> curr;
+               for (int count = 0; count < i.second; count++) {
+                   curr.push_back(i.first);
+                   subset.push_back(curr);
+               }
+
+               // create subsets from previous entries
+               for (auto entry : result) {
+                  for (auto j : curr) {
+                     entry.push_back(j);
+                     subset.push_back(entry);
+                  }
+               }
+
+               // update result with computed subsets
+               for (auto& k : subset)
+                  result.push_back(k);
+            }
+
+            // empty Set
+            result.push_back(vector<int>());
+
+            return result;
+        }
+
         void OutputResult(vector<vector<int>>& result) const {
            cout << "Subsets :" << endl;
            for (auto i : result) {
@@ -80,8 +133,13 @@ int main(void) {
     //vector<int> v = {1,2,3};
     //vector<int> v = {1,2,3,4};
     //vector<int> v = {1,2,3,4,5,6};
-    vector<int> v = {};
+    //vector<int> v = {};
+    vector<int> v = {1,1,1,2,2};
+#if NODUPLICATES
     auto ans = sol.subsets(v);
+#else
+    auto ans = sol.subsetsForDuplicates(v);
+#endif    
     sol.OutputResult(ans);
     cout << "Total elements :" << v.size() 
          << " Total subsets :" << ans.size() << endl;
